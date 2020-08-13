b942dd2aee3877bf7f52812c35798fed258df112
Christia <ChristiaTidwell@gmail.com>
2020-08-12 21:01:54 -0500

    Updated error method

--- ProjectApp/Controllers/HomeController.cs
+++ ProjectApp/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ProjectApp.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -26,10 +28,12 @@ namespace ProjectApp.Controllers
             return View();
         }
 
+        
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View();
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-
     }
+
 }
