732b20a6551e8a696f1f4c7570d2212819fb3a06
Christia <ChristiaTidwell@gmail.com>
2020-08-15 18:43:49 -0500

    catching up commits

---
+++ ProjectApp/Models/ProjectDBContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+public class ProjectDBContext : DbContext
+    {
+        public ProjectDBContext (DbContextOptions<ProjectDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjectApp.Models.ProjectViewModel> ProjectViewModel { get; set; }
+    }
