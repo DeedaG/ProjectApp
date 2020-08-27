886a3568167da53f39f67b1c1cc6bf4bb3c042a0
Christia <ChristiaTidwell@gmail.com>
2020-08-27 11:15:37 -0500

    Testing seeding and foreigh constraints

---
+++ ProjectApp/Migrations/ProjectDB/20200827145008_Initial Create.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectApp.Migrations.ProjectDB
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ApplicationUser",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    UserName = table.Column<string>(nullable: true),
+                    NormalizedUserName = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    NormalizedEmail = table.Column<string>(nullable: true),
+                    EmailConfirmed = table.Column<bool>(nullable: false),
+                    PasswordHash = table.Column<string>(nullable: true),
+                    SecurityStamp = table.Column<string>(nullable: true),
+                    ConcurrencyStamp = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    PhoneNumberConfirmed = table.Column<bool>(nullable: false),
+                    TwoFactorEnabled = table.Column<bool>(nullable: false),
+                    LockoutEnd = table.Column<DateTimeOffset>(nullable: true),
+                    LockoutEnabled = table.Column<bool>(nullable: false),
+                    AccessFailedCount = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApplicationUser", x => x.Id);
+                });
+
+
+            migrationBuilder.InsertData(
+                table: "ChartData",
+                columns: new[] { "Id"},
+                values: new object[] { 1 });
+
+            migrationBuilder.InsertData(
+                table: "ProjectViewModel",
+                columns: new[] { "Id", "EndDate", "Info", "Language", "Name", "ProjectDataId", "StartDate" },
+                values: new object[] { 1, null, "test1", "Test1", "Test1", 1, null });
+
+            migrationBuilder.InsertData(
+                table: "ProjectViewModel",
+                columns: new[] { "Id", "EndDate", "Info", "Language", "Name", "ProjectDataId", "StartDate" },
+                values: new object[] { 2, null, "test2", "Test2", "Test2", 1, null });
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ApplicationUser");
+        }
+    }
+}
---
+++ ProjectApp/Models/ModelBuilderExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjectApp.Models
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProjectViewModel>().HasData(
+                new ProjectViewModel
+                {
+                    Id = 1,
+                    Name = "Test1",
+                    Language = "Test1",
+                    Info = "test1",
+                    StartDate = null,
+                    EndDate = null,
+                    //ProjectUserId = "1",
+                    ProjectDataId = 1
+                },
+
+            new ProjectViewModel
+            {
+                Id = 2,
+                Name = "Test2",
+                Language = "Test2",
+                Info = "test2",
+                StartDate = null,
+                EndDate = null,
+                //ProjectUserId = "1",
+                ProjectDataId = 1
+            });
+
+            modelBuilder.Entity<ChartData>().HasData(
+                new ChartData
+                {
+                    Id = 1,
+                    //ChartUserId = "1"
+                });
+        }
+    }
+}
