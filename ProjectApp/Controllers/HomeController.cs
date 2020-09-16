c1dbefde1766643ee34e74a908d2814b0ff5d5db
Christia <ChristiaTidwell@gmail.com>
2020-09-16 17:12:00 -0500

    * UserPrefs.xml: * ProjectApp.dll: * ProjectApp.pdb: * ProjectApp.dll: * ProjectApp.pdb: * ProjectApp.Views.dll: * ProjectApp.Views.pdb: * ProjectApp.Views.dll: * ProjectApp.Views.pdb: * _PieChartData.cshtml.g.cs:
    * HomeController.cs: Renamed variables, grouped dynamic pie data
    
    * _PieChartData.cshtml: simplifed dynamic data for days coding

--- ProjectApp/Controllers/HomeController.cs
+++ ProjectApp/Controllers/HomeController.cs
@@ -36,46 +36,65 @@ namespace ProjectApp.Controllers
                 .ToListAsync();
 
             var langTypes = projects.GroupBy(x => x.Language).ToList();
-
             var freqOrder = new List<string>();
-
             var freqData = langTypes.ToDictionary(x => x.Key.ToString(), x => x.Count());
-
             foreach (var item in freqData)
             {
                 freqOrder.Add(@"[""" + item.Key + @"""," + item.Value + @"]");
             }
-
             var resultData = new List<string>();
-
             for (int i = 0; i < freqOrder.Count - 1; i++)
             {
                 resultData.Add(freqOrder[i] + @",");
             }
-
             resultData.Add(freqOrder.Last());
             resultData.Insert(0, @"[""Language"", ""Frequency""],");
 
-            var projDates = new List<KeyValuePair<string, string>>();
 
+            var projTime = new List<KeyValuePair<string, string>>();
             foreach (var p in projects)
             {
-                projDates.Add(new KeyValuePair<string, string>(p.Name.ToString(), p.StartDate?.ToString("MM/dd/yyyy")));
+                if (p.EndDate != null)
+                {
+                    TimeSpan? difference = p.EndDate - p.StartDate;
+                    projTime.Add(new KeyValuePair<string, string>(p.Language.ToString(), difference.Value.TotalDays.ToString()));
+                }
             }
-
             var resultData2 = new List<string>();
+            for (int i = 0; i < projTime.Count - 1; i++)
+            {
+                resultData2.Add(@"[""" + projTime[i].Key + @"""," + projTime[i].Value + "]" + ",");
+            }
+            if (projTime.Count >= 1)
+            {
+                resultData2.Add(@"[""" + projTime[projTime.Count - 1].Key + @"""," + projTime[projTime.Count - 1].Value + "]");
+            }
+            resultData2.Insert(0, @"[""Language"", ""Days Coding""],");
 
+
+            var projDates = new List<KeyValuePair<string, string>>();
+            foreach (var p in projects)
+            {
+                if (p.EndDate == null) {
+                    projDates.Add(new KeyValuePair<string, string>(p.Name.ToString(), p.StartDate?.ToString("MM/dd/yyyy")));
+                }
+            }
+            var resultData3 = new List<string>();
                 for (int i = 0; i < projDates.Count - 1; i++)
                 {
-                    resultData2.Add(@"[""" + projDates[i].Key + @"""," + projDates[i].Value + "]" + ",");
+                    resultData3.Add(@"[""" + projDates[i].Key + @"""," + projDates[i].Value + "]" + ",");
                 }
+            if (projDates.Count >= 1)
+            {
+                resultData3.Add(@"[""" + projDates[projDates.Count - 1].Key + @"""," + projDates[projDates.Count - 1].Value + "]");
+            }
+            resultData3.Insert(0, @"[""Name"", ""Start Date""],");
 
-            resultData2.Add(@"[""" + projDates[projects.Count - 1].Key + @"""," + projDates[projects.Count - 1].Value + "]");
-            resultData2.Insert(0, @"[""Name"", ""Start Date""],");
 
             dynamic mymodel = new ExpandoObject();
             mymodel.resultData = resultData;
             mymodel.resultData2 = @resultData2;
+            mymodel.resultData3 = @resultData3;
             mymodel.langTypes = langTypes;
             return View(mymodel);
         }
