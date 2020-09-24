283bbd94b9674b49f0d189bbca775afc0888c3e6
Christia <ChristiaTidwell@gmail.com>
2020-09-24 12:14:49 -0500

    * UserPrefs.xml: Changed icon size/ removed background style
    * Index.cshtml:
    * ProjectApp.dll:
    * ProjectApp.pdb:
    * ReportJournalController.cs:
    * ProjectApp.dll:
    * ProjectApp.pdb:
    * ProjectApp.Views.dll:
    * ProjectApp.Views.pdb:
    * ProjectApp.Views.dll:
    * ProjectApp.Views.pdb:
    * Index.cshtml.g.cs:

--- ProjectApp/Controllers/ReportJournalController.cs
+++ ProjectApp/Controllers/ReportJournalController.cs
@@ -21,8 +21,6 @@ namespace ProjectApp.Controllers
         // GET: ReportJournal
         public async Task<IActionResult> Index()
         {
-            
-            //var projects = await _context.ReportJournal.Where(x => x.DevTime.Equals(0)).ToListAsync();
             var reports = await _context.ReportJournal.AsNoTracking().ToListAsync();
             var reports1 = new List<ProjectApp.Models.ReportJournal>();
             foreach (var item in reports)
